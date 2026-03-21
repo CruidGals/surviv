8a4b5a8dbfc0e0ba517f7c06469ec54225d69474
Khai Ta <khaita2006@gmail.com>
2026-03-21 15:11:04 -0400

    create xcode project

---
+++ surviv/survivTests/survivTests.swift
@@ -0,0 +1,16 @@
+//
+//  survivTests.swift
+//  survivTests
+//
+//  Created by Khai Ta on 3/21/26.
+//
+
+import Testing
+
+struct survivTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
